fea036f93dfe5e16fad8ef8672dabbeb0cd732f5
Harun Davood <davoodharun@gmail.com>
2016-05-12 10:24:28 -0400

    added map styling

--- src/mobile/client/www/js/controllers/HubDetailController.js
+++ src/mobile/client/www/js/controllers/HubDetailController.js
@@ -3,6 +3,31 @@
     .controller('HubDetailController', function($scope, $stateParams, Hubs, Authorization, $location) {
     	var vm = this;
         vm.hub;
+        vm.mapStyle = [
+                        {
+                          stylers:[
+                            {hue:'#28C948'},
+                            {visibility:'simplified'},
+                            {gamma:0.5},
+                            {weight:0.5}
+                          ]
+                        },
+                        {
+                          elementType:'labels',
+                          stylers:[
+                            {visibility:'on'},
+                          ]
+                        },
+                        {
+                          featureType:'water',
+                          stylers:[
+                            {color:'#A1E0FD'}
+                          ]
+                        },
+                        { 
+                          "featureType": "landscape.natural",
+                           "stylers": [ { "visibility": "on" }, { "color": "#28C948" }, { "gamma": 4.97 }, { "lightness": -5 }, { "saturation": 100 } ] }
+                      ]
         // check if user is logged in; if not, navigate to login
     	Authorization.isLoggedIn().then(function(response) {
     	  if(!response.data) {
--- src/mobile/client/www/js/controllers/StartController.js
+++ src/mobile/client/www/js/controllers/StartController.js
@@ -3,28 +3,31 @@
     .controller('StartController', function($scope, $timeout, Authorization, hubSearch, $location, NgMap) {
       console.log('DashCtrl initialized')
       var vm = this;
-        // NgMap.getMap().then(function(map) {
-        //     vm.map = map;
-        //   });
-
-        // vm.stores = {
-        //   foo: { position:[41, -87], items: [1,2,3,4]},
-        //   bar:{ position:[41, -83], items: [5,6,7,8]}
-        // };
-
-        // vm.googleMapsUrl = 'https://maps.google.com/maps/api/js';
-        // vm.pauseLoading=true;
-        // console.log("Starting a timer to wait for 2 seconds before the map will start loading");
-
-        // $timeout(function() {
-        //   console.debug("Showing the map. The google maps api should load now.");
-        //   vm.pauseLoading=false;
-        // }, 2000);
-
-        // vm.showStore = function(evt, id) {
-        //   vm.store = vm.stores[id];
-        //   vm.map.showInfoWindow('foo', this);
-        // };
+      vm.mapStyle = [
+                      {
+                        stylers:[
+                          {hue:'#28C948'},
+                          {visibility:'simplified'},
+                          {gamma:0.5},
+                          {weight:0.5}
+                        ]
+                      },
+                      {
+                        elementType:'labels',
+                        stylers:[
+                          {visibility:'on'},
+                        ]
+                      },
+                      {
+                        featureType:'water',
+                        stylers:[
+                          {color:'#A1E0FD'}
+                        ]
+                      },
+                      { 
+                        "featureType": "landscape.natural",
+                         "stylers": [ { "visibility": "on" }, { "color": "#28C948" }, { "gamma": 4.97 }, { "lightness": -5 }, { "saturation": 100 } ] }
+                    ]
    
       // check if user is logged in; if not, navigate to login
       Authorization.isLoggedIn().then(function(response) {
